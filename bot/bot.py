214f59dc5ac67497a9de23bff7f59bac6f3bc122
ardelagi <ardetmg031@gmail.com>
2025-07-30 02:56:12 +0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
 from dotenv import load_dotenv
 from utils.auction_manager import AuctionManager
 
@@ -13,18 +14,27 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 auction_manager = AuctionManager(bot)
 
-@bot.event
-async def on_ready():
-    print(f'Bot {bot.user.name} siap!')
+async def load_extensions():
     await bot.load_extension('commands.startauction')
     await bot.load_extension('commands.stopauction')
     await bot.load_extension('commands.setauctiontimer')
+
+@bot.event
+async def on_ready():
+    print(f'Bot {bot.user.name} siap!')
     
+    
+    await load_extensions()
+    
+
     synced = await bot.tree.sync()
     print(f"Command tersinkronisasi: {len(synced)}")
     
-    await bot.load_extension('commands.startauction')
-    
+
+    commands_list = await bot.tree.fetch_commands()
+    print("Command terdaftar:")
+    for cmd in commands_list:
+        print(f"- {cmd.name}")
 
 @bot.event
 async def on_message(message):
@@ -35,4 +45,4 @@ async def on_message(message):
     await bot.process_commands(message)
 
 if __name__ == "__main__":
-    bot.run(os.getenv('DISCORD_TOKEN'))
+    asyncio.run(bot.start(os.getenv('DISCORD_TOKEN')))

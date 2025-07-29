80880bb4b921024b5e5f559d72516a1f8519f01c
ardelagi <ardetmg031@gmail.com>
2025-07-30 03:47:18 +0700

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,27 +1,29 @@
-import os
 from flask import Flask, render_template
 from controllers.auction_controller import auction_bp
+import os
 from config import Config
-from models.auction import AuctionModel
+from bson import ObjectId
 
-template_dir = os.path.abspath('views/templates')
-static_dir = os.path.abspath('views/static')
+app = Flask(__name__)
+app.config.from_object(Config)
+app.register_blueprint(auction_bp)
 
-app = Flask(__name__, 
-            template_folder=template_dir,
-            static_folder=static_dir)
-
+# Jinja filter untuk format Rupiah
 @app.template_filter('format_rupiah')
 def format_rupiah_filter(value):
-    if value >= 1000000:
-        return f"Rp{value / 1000000:.1f}jt".replace('.0', '')
-    elif value >= 1000:
-        return f"Rp{value / 1000:.0f}rb"
-    return f"Rp{value:,}".replace(',', '.')
+    try:
+        value = int(value)
+        if value >= 1000000:
+            return f"Rp{value / 1000000:.1f}jt".replace('.0', '')
+        elif value >= 1000:
+            return f"Rp{value / 1000:.0f}rb"
+        return f"Rp{value:,}".replace(',', '.')
+    except:
+        return f"Rp{value}"
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3002, debug=True)

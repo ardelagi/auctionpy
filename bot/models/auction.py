7cfeb5fd2f97d9ab7864348ee35e85dc3e01ecc2
ardelagi <ardetmg031@gmail.com>
2025-07-30 03:44:04 +0700

    Update auction.py

--- bot/models/auction.py
+++ bot/models/auction.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
+from bson import ObjectId
 from config import MONGO_URI, DB_NAME
-import time
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
@@ -11,22 +11,28 @@ class AuctionModel:
     @staticmethod
     def create_auction(data):
         return auctions.insert_one(data).inserted_id
-        
-    @staticmethod
-    def get_auction(auction_id):
-        return auctions.find_one({'_id': auction_id})
 
     @staticmethod
     def update_auction(auction_id, update_data):
-        return auctions.update_one({'_id': auction_id}, {'$set': update_data})
+        return auctions.update_one(
+            {'_id': ObjectId(auction_id)}, 
+            {'$set': update_data}
+        )
 
     @staticmethod
     def get_active_auction(channel_id):
         return auctions.find_one({'channel_id': channel_id, 'isActive': True})
 
     @staticmethod
+    def get_auction(auction_id):
+        return auctions.find_one({'_id': ObjectId(auction_id)})
+    
+    @staticmethod
     def stop_auction(auction_id):
-        return auctions.update_one({'_id': auction_id}, {'$set': {'isActive': False}})
+        return auctions.update_one(
+            {'_id': ObjectId(auction_id)}, 
+            {'$set': {'isActive': False}}
+        )
     
     @staticmethod
     def set_default_timer(guild_id, timer):
@@ -39,4 +45,4 @@ class AuctionModel:
     @staticmethod
     def get_default_timer(guild_id):
         setting = guild_settings.find_one({'guild_id': guild_id})
-        return setting.get('default_timer') if setting else None
+        return setting['default_timer'] if setting else None

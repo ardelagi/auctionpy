ff8e8aa6611b9f67e8ea3b2fa26e116b03a77ade
ardelagi <ardetmg031@gmail.com>
2025-07-30 03:48:05 +0700

    Update auction.py

--- dashboard/models/auction.py
+++ dashboard/models/auction.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from config import Config
+from bson import ObjectId
 
 client = MongoClient(Config.MONGO_URI)
 db = client[Config.DB_NAME]
@@ -16,11 +17,17 @@ class AuctionModel:
     
     @staticmethod
     def get_auction(auction_id):
-        return auctions_collection.find_one({'_id': auction_id})
+        try:
+            return auctions_collection.find_one({'_id': ObjectId(auction_id)})
+        except:
+            return None
     
     @staticmethod
     def stop_auction(auction_id):
-        return auctions_collection.update_one(
-            {'_id': auction_id},
-            {'$set': {'isActive': False}}
-        )
+        try:
+            return auctions_collection.update_one(
+                {'_id': ObjectId(auction_id)},
+                {'$set': {'isActive': False}}
+            )
+        except:
+            return None

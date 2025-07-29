15de7d9087b7a559112d54d94fc241a68915c78d
ardelagi <ardetmg031@gmail.com>
2025-07-30 03:43:38 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,4 +7,4 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 MONGO_URI = os.getenv('MONGO_URI')
 DB_NAME = os.getenv('DB_NAME', 'auction_bot')
 ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID', 0))
-DEFAULT_TIMER = int(os.getenv('DEFAULT_TIMER', 300))  # 5 menit default
+DEFAULT_TIMER = int(os.getenv('DEFAULT_TIMER', 300))

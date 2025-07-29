77f6444bcb7d01d2c801bf17a4eb9cc68cf87b2e
ardelagi <ardetmg031@gmail.com>
2025-07-30 03:44:29 +0700

    Update formatter.py

--- bot/utils/formatter.py
+++ bot/utils/formatter.py
@@ -1,6 +1,10 @@
 def format_rupiah(amount):
-    if amount >= 1000000:
-        return f"Rp{amount / 1000000:.1f}jt".replace('.0', '')
-    elif amount >= 1000:
-        return f"Rp{amount / 1000:.0f}rb"
-    return f"Rp{amount:,}".replace(',', '.')
+    try:
+        amount = int(amount)
+        if amount >= 1000000:
+            return f"Rp{amount / 1000000:.1f}jt".replace('.0', '')
+        elif amount >= 1000:
+            return f"Rp{amount / 1000:.0f}rb"
+        return f"Rp{amount:,}".replace(',', '.')
+    except:
+        return f"Rp{amount}"
